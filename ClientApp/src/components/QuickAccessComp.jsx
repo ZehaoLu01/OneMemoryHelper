3ef7e822f90d913a4f9244ed98a36775a7f78a28
Zehao Lu <luzehao1998@gmail.com>
2023-05-18 00:46:36 -0700

    complete quick search component draft

--- ClientApp/src/components/QuickAccessComp.jsx
+++ ClientApp/src/components/QuickAccessComp.jsx
@@ -1,9 +1,11 @@
 ﻿import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
+import Grid from '@mui/material/Grid'
 
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import GoogleLogo from '../images/Google_logo.png'
+import ShortcutCard from './ShortcutCard';
 
 
 export default function QuickAccessComp() {
@@ -16,13 +18,34 @@ export default function QuickAccessComp() {
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(gcse, s);
     });
+    const sizeOfShortcuts = 8;
+    const [shortcutUrls, setShortcutUrls] = useState(['https://www.google.com','','','','','','','']);
+
+    const generateShortcutCard = () => {
+        let cards = [];
+        console.log("test");
+        for (let i = 0; i < sizeOfShortcuts; i++) {
+            cards.push(
+                <Grid item key={i} xs={3} >
+                    <ShortcutCard url={shortcutUrls[i]} ></ShortcutCard>
+                </Grid>
+            )
+        }
+        return cards;
+    };
 
     return (
-        <Stack direction="column" alignItems="center">
-            <img src={GoogleLogo} />
-            <Box sx={{ width: "100%"} }>
+        <Grid container direction="column" alignItems="center" spacing={3}>
+            <Grid item>
+                <img src={GoogleLogo} />
+            </Grid>
+            <Grid item sx={{ width: "80%"} }>
                 <div className="gcse-searchbox-only" datanewwindow="true"></div>
-            </Box>
-        </Stack>
+            </Grid>
+            <Grid item container xs sx={{ width: '80%' }} spacing={3}>
+                {generateShortcutCard()}
+            </Grid>
+           
+        </Grid>
     )
 }
