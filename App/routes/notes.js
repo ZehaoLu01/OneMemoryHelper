05e104f0796655f715ea7388a5f98d862981befc
Zehao Lu <luzehao1998@gmail.com>
2023-07-20 16:02:55 -0700

    extract recent modified logic to service level

--- App/routes/notes.js
+++ App/routes/notes.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-var axios = require("axios");
 var userServices = require("../services/user");
 var noteServices = require("../services/note");
 
@@ -12,49 +11,16 @@ router.use("/recentlyModifiedNotes", async function (req, res, next) {
   try {
     if (req.session.isAuthorized === true) {
       const userId = req.session.idTokenClaims.oid;
+      const userData = (await userServices.getUserDataById(userId))[0];
 
       //==============================
-      const userData = (await userServices.getUserDataById(userId))[0];
-      const lastUpdateTime = new Date(userData.lastUpdateTime);
-      let requestURL = `https://graph.microsoft.com/v1.0/me/onenote/pages?$filter=lastModifiedDateTime ge ${
-        isTesting
-          ? req.query.lastModifiedDateTime
-          : lastUpdateTime.toISOString()
-      }`;
-      let fetchedNotes = [];
-
-      while (requestURL !== undefined) {
-        const noteResponse = await axios.get(requestURL, {
-          headers: { Authorization: "Bearer " + req.session.accessToken },
-        });
-
-        requestURL = undefined;
-        if (noteResponse.data) {
-          fetchedNotes = fetchedNotes.concat(noteResponse.data.value);
-          if (noteResponse.data["@odata.nextLink"]) {
-            requestURL = noteResponse.data["@odata.nextLink"];
-          }
-        }
-      }
-
-      let insertedData = [];
-      let showingData = [];
-      if (fetchedNotes.length != 0) {
-        insertedData = await noteServices.upsertNotes(
-          fetchedNotes,
-          req.session.idTokenClaims?.oid
-        );
-      }
-
-      if (insertedData.length != 0) {
-        userServices.updateLastUpdateTimeToNow(userData.id);
-      }
-
-      // We don't want to show notes that is being reviewed, so we need to get rid of those currently getting reviewed.
-      // We also want to show these notes for multiple days instead of once.
-      showingData = await noteServices.filterFetchedNotes(
-        fetchedNotes.map((note) => note.id)
+      let showingData = await noteServices.getNotesForFrontendAndUpdate(
+        userData,
+        isTesting,
+        req
       );
+
+      userServices.updateLastUpdateTimeToNow(userData.id);
 
       res.json({ value: showingData });
     } else {
--- App/services/note.js
+++ App/services/note.js
@@ -1,6 +1,6 @@
 const noteData = require("../models/notes");
-
 const config = require("../config");
+var axios = require("axios");
 
 const noteServices = {};
 
@@ -50,6 +50,48 @@ noteServices.filterFetchedNotes = async (noteIds) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+noteServices.getNotesForFrontendAndUpdate = async function (
+  userData,
+  isTesting,
+  req
+) {
+  const lastUpdateTime = new Date(userData.lastUpdateTime);
+  let requestURL = `https://graph.microsoft.com/v1.0/me/onenote/pages?$filter=lastModifiedDateTime ge ${
+    isTesting ? req.query.lastModifiedDateTime : lastUpdateTime.toISOString()
+  }`;
+  let fetchedNotes = [];
+
+  while (requestURL !== undefined) {
+    const noteResponse = await axios.get(requestURL, {
+      headers: { Authorization: "Bearer " + req.session.accessToken },
+    });
+
+    requestURL = undefined;
+    if (noteResponse.data) {
+      fetchedNotes = fetchedNotes.concat(noteResponse.data.value);
+      if (noteResponse.data["@odata.nextLink"]) {
+        requestURL = noteResponse.data["@odata.nextLink"];
+      }
+    }
+  }
+
+  let insertedData = [];
+  let showingData = [];
+  if (fetchedNotes.length != 0) {
+    insertedData = await noteServices.upsertNotes(
+      fetchedNotes,
+      req.session.idTokenClaims?.oid
+    );
+  }
+
+  // We don't want to show notes that is being reviewed, so we need to get rid of those currently getting reviewed.
+  // We also want to show these notes for multiple days instead of once.
+  showingData = await noteServices.filterFetchedNotes(
+    fetchedNotes.map((note) => note.id)
+  );
+  return showingData;
 };
 
 module.exports = noteServices;
