9b42500a2b9b203451273c465ab567881c898c92
Zehao Lu <luzehao1998@gmail.com>
2023-07-23 10:57:30 -0700

    avatar update

--- App/client/src/components/Nav.jsx
+++ App/client/src/components/Nav.jsx
@@ -18,6 +18,35 @@ import { authorizationContext } from '../context';
 
 const pages = ['Tasks', 'Weather', 'Planner'];
 
+function stringToColor(string) {
+    let hash = 0;
+    let i;
+  
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+  
+    let color = '#';
+  
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+  
+    return color;
+}
+  
+function stringAvatar(name) {
+return {
+    sx: {
+    bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+};
+}
+
 export default function Nav() {
     const [anchorElNav, setAnchorElNav] = useState(null);
     const [anchorElUser, setAnchorElUser] = useState(null);
@@ -158,7 +187,7 @@ export default function Nav() {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                <Avatar {...stringAvatar("Zehao Lu")}></Avatar>
                             </IconButton>
                         </Tooltip>
                         <Menu
